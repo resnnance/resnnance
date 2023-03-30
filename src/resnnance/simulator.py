898c91ce552f1322387150bf8744b5a4caa8dae2
slopez <slopez@die.upm.es>
2023-03-30 18:07:33 +0200

    Add simulator singleton + API functions

--- src/resnnance/simulator.py
+++ src/resnnance/simulator.py
@@ -1,9 +1,34 @@
 from pyNN.common.control import BaseState
 
-class ReSNNance(BaseState)
-"""
-ReSNNance PyNN simulator interface
-"""
+from typing import Optional
+import logging
+
+class ReSNNance(BaseState):
+    """
+    ReSNNance PyNN simulator interface
+    """
 
     def __init__(self):
         super().__init__()
+
+        # Simulator logger
+        self.logger = logging.getLogger("resnnance")
+
+        self.t = 0
+
+
+    def clear(self):
+        """
+        Clear the simulator structure
+        """
+
+
+    def run_until():
+        """
+        Runs the simulator
+        """
+
+
+# ReSNNance simulator singleton object (instantiated in setup())
+# Optional[] is a type hint: state can be a ReSNNance object or None
+state: Optional[ReSNNance] = None

f7e8c9154e155bdf728b78bf398b7e3c5908766f
slopez <>
2023-09-24 19:40:00 +0200

    Add layers

--- src/resnnance/core/layers.py
+++ src/resnnance/core/layers.py
@@ -1,13 +1,57 @@
 class Layer(object):
-    pass
 
-class Conv2D(Layer):
-    pass
+    def __init__(self, weights=None):
+        raise NotImplementedError
 
-class Pooling(Layer):
-    pass
+    def set_layer(self, info):
+        raise NotImplementedError
 
 class Dense(Layer):
 
-    def __init__(self, weights):
-        self.weights = weights
+    def __init__(self, weights=None):
+        if weights == None:
+            self.weights = None
+        else:
+            self.set_layer(weights)
+    
+    def set_layer(self, info):
+        if self.weights.shape == (2,2):
+            self.weights = info         # Dense weight matrix
+        else:
+            raise ValueError('Wrong weight matrix shape')
+
+class Conv2D(Layer):
+
+    def __init__(self, info=None):
+        if info == None:
+            self.n = None
+            self.stride = None
+            self.padding = None
+            self.kernel = None
+        else:
+            self.set_layer(info)
+
+    def set_layer(self, info):
+        self.n = info.n                 # Input dimensions (y,x,z)
+        self.stride = info.stride       # Stride steps (y,x)
+        self.padding = info.padding     # Padding (valid, none)
+
+        if info.kernel.shape[2] == 1:   # z dim = 1 => Only 2D kernels
+            self.kernel = info.kernel   # Array of kernel weight matrices
+        else:
+            raise ValueError('Wrong kernel matrix')
+
+class Pooling(Layer):
+
+    def __init__(self, info=None):
+        if info == None:
+            self.n = None
+            self.stride = None
+            self.pool = None
+        else:
+            self.set_layer(info)
+
+    def set_layer(self, info):
+        self.n = info.n                 # Input dimensions (y,x,z)
+        self.stride = info.stride       # Stride steps (y,x)
+        self.pool = info.pool           # Pool size (y,x)
